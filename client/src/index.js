9c7429976d5060cbdf1673aaeab940a4813a01b5
unknown <pmartinsdev@gmail.com>
2019-07-23 11:53:44 -0500

    moved img folder to client-side. deleted files not needed

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './Containers/App/App'
+import { createGlobalStyle } from 'styled-components'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+body {
+    background-image: url('');
+    background: rgb(255,255,255);
+    background: linear-gradient(90deg, rgba(255,255,255,0.5) 0%, rgba(255,255,255,0.5) 44%, rgba(118,189,7,0.5) 100%);
+}
+`
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root')
+)
