0572a0e2951f779d900fd5bfca82bdfaec7cdef5
Sal Allegra <salallegra@gmail.com>
2019-07-24 01:38:01 -0500

    Remove bad imports

--- server/src/main/java/com/matchaleaf/filesystem/entity/File.java
+++ server/src/main/java/com/matchaleaf/filesystem/entity/File.java
@@ -2,30 +2,26 @@ package com.matchaleaf.filesystem.entity;
 
 import javax.persistence.*;
 
-import com.cooksys.orm.entity.Major;
-import com.cooksys.orm.entity.Student;
-
 import java.util.Set;
 
 @Entity
 public class File {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @Column(unique = true)
-    private String name;
+	@Column(unique = true)
+	private String name;
 
-    @ManyToOne
-    private Folder folder;
+	@ManyToOne
+	private Folder folder;
 
-    public File() {
-    }
+	public File() {
+	}
 
 //    public File(String name) {
 //        this.name = name;
 //    }
-
 
 }
