f7630b7463b30ff59ba4853958f768aec6fc5e43
Sal Allegra <salallegra@gmail.com>
2019-07-25 14:36:43 -0500

    Add another method stub

--- server/src/main/java/com/matchaleaf/filesystem/services/FileService.java
+++ server/src/main/java/com/matchaleaf/filesystem/services/FileService.java
@@ -2,23 +2,23 @@ package com.matchaleaf.filesystem.services;
 
 import java.util.List;
 
-
 import com.matchaleaf.filesystem.dto.*;
 
 public interface FileService {
 
-	//The dto's we need
-    //	IDResponseDto
+	// The dto's we need
+	// IDResponseDto
 //	FileUploadDto
 //	FolderUploadDto
 //	FileDownloadDto --This will have the bytes the actual bytes as well
 //	FileDto   ---This is related to the photo Pedro sent me in slack
 //	FolderDto
 //
+
+	//This is can also be considered uploading a file
+	IdResponseDto createFile(FileUploadDto fileUploadDto);   
+	FileDownloadDto downloadFileById(Integer id);
 	
-	  FileDownloadDto downloadFileById(Integer id);
-	  
-	  
-	  
-	  
+	
+
 }
