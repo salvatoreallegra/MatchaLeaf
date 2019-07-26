482c6d173abd225f43974f6aac4cbd9b1a46d6f9
unknown <pmartinsdev@gmail.com>
2019-07-26 14:57:25 -0500

    upload btn implemented

--- client/src/Components/File/index.js
+++ client/src/Components/File/index.js
@@ -6,7 +6,14 @@ import trashIcon from '../../image_source/trashIcon.png'
 const File = files => (
   <div>
     {files.map(file => (
-      <div key={file.id} style={{ background: 'white', margin: '10px' }}>
+      <div
+        key={file.id}
+        style={{
+          background: 'white',
+          margin: '10px',
+          boxShadow: '1px 1px 5px rgba(000, 000, 000, 0.3)'
+        }}
+      >
         <img src={fileIcon} alt='file' />
         {file.name}
         <img src={downloadIcon} alt='download' />
---
+++ client/src/Components/UploadButton/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Btn from '../../Elements/Btn'
+
+const UploadButton = openModal => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'cross-start',
+      gridRowStart: '5',
+      gridRowEnd: '5',
+      gridColumnStart: '5',
+      gridColumnEnd: '5'
+    }}
+  >
+    <Btn type='button' onClick={openModal} />
+  </div>
+)
+
+export default UploadButton
