05cb23548d388309aadcef92d2533b7d36e51fbc
unknown <pmartinsdev@gmail.com>
2019-07-29 10:47:07 -0500

    created action creators for both ducks and installed axios

--- client/src/ducks/folder.duck.js
+++ client/src/ducks/folder.duck.js
@@ -1,6 +1,9 @@
 import initialState from './mockState'
 
 // actions
+export const LOAD_FOLDER = 'cooksys/matchaleaf/Folder/LOAD_FOLDER'
+export const CREATE_FOLDER = 'cooksys/matchaleaf/Folder/CREATE_FOLDER'
+export const DELETE_FOLDER = 'cooksys/matchaleaf/Folder/DELETE_FOLDER'
 
 // initial state
 // const initialState = {
@@ -13,9 +16,39 @@ import initialState from './mockState'
 // reducer
 export default function reducer (state = initialState, action) {
   switch (action.type) {
+    case LOAD_FOLDER:
+      return {
+        ...state,
+        id: action.payload.id,
+        name: action.payload.name,
+        files: action.payload.files,
+        folders: action.payload.folders
+      }
+    case CREATE_FOLDER:
+      return {
+        ...state
+      }
+    case DELETE_FOLDER:
+      return {
+        ...state
+      }
     default:
       return state
   }
 }
 
 // action creators
+export const loadFolder = folder => ({
+  type: LOAD_FOLDER,
+  payload: folder
+})
+
+export const createFolder = folder => ({
+  type: CREATE_FOLDER,
+  payload: folder
+})
+
+export const deleteFolder = folderId => ({
+  type: DELETE_FOLDER,
+  payload: folderId
+})
