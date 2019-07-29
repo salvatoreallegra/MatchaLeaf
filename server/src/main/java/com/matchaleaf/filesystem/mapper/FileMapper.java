a80bad944d370a6fda0e859a2a2c2ae0ef59360a
Thabani Chibanda <tchibanda24@gmail.com>
2019-07-29 10:42:22 -0500

    FileMapper add fileToFileDto #75

--- server/src/main/java/com/matchaleaf/filesystem/mapper/FileMapper.java
+++ server/src/main/java/com/matchaleaf/filesystem/mapper/FileMapper.java
@@ -6,11 +6,13 @@ import com.matchaleaf.filesystem.entity.File;
 
 @Mapper(componentModel = "spring")
 public interface FileMapper {
-
+	
+	
 	// Convert our file upload dto to a file entity
 	File dtoToEntity(FileUploadDto fileUploadDto);
 
 	// This will return the IdResponse to File Service
 	IdResponseDto entityToDto(File file);
-
+	
+	FileDto fileToFileDto(File file);
 }
