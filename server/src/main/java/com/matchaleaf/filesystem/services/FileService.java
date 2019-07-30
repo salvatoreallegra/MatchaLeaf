f9c3f4e840a2185d72aed83b612db4310f2ddc38
Sal Allegra <salallegra@gmail.com>
2019-07-29 19:19:27 -0500

    unknown

--- server/src/main/java/com/matchaleaf/filesystem/services/FileService.java
+++ server/src/main/java/com/matchaleaf/filesystem/services/FileService.java
@@ -1,6 +1,5 @@
 package com.matchaleaf.filesystem.services;
 
-
 import java.util.List;
 
 import org.springframework.core.io.Resource;
@@ -10,29 +9,19 @@ import org.springframework.web.multipart.MultipartFile;
 import com.matchaleaf.filesystem.entity.*;
 
 import com.matchaleaf.filesystem.dto.*;
+
 @Service
 public interface FileService {
 
-// The dto's we need
-// IDResponseDto
-//	FileUploadDto
-//	FolderUploadDto
-//	FileDownloadDto --This will have the bytes the actual bytes as well
-//	FileDto   ---This is related to the photo Pedro sent me in slack
-//	FolderDto
-//
-
-	//This is can also be considered uploading a file
+	// This can also be considered uploading a file
 	IdResponseDto createFile(MultipartFile file, Integer folderId);
-	
-	
+
 	FileDownloadDto downloadFileById(Integer id);
-	
+
 	FileDto trashFile(Integer Id);
-	
+
 	File downloadFileBytesById(Integer id);
 
-	FileDto moveFile(Integer id, String name);	
-	
+	FileDto moveFile(Integer id, String name);
 
 }
--- server/src/main/java/com/matchaleaf/filesystem/services/FolderService.java
+++ server/src/main/java/com/matchaleaf/filesystem/services/FolderService.java
@@ -10,41 +10,22 @@ import com.matchaleaf.filesystem.dto.IdResponseDto;
 
 @Service
 public interface FolderService {
-	
-// The dto's we need
-//      IDResponseDto
-//		FileUploadDto
-//		FolderUploadDto
-//		FileDownloadDto --This will have the bytes the actual bytes as well
-//		FileDto   ---This is related to the photo Pedro sent me in slack
-//		FolderDto
-	
-	   IdResponseDto createFolder(FolderUploadDto folderUploadDto);
-	   
-	   FolderDto getFolderById(Integer id);
-	   
-	   
-	   
-	   /*
-	    * The json response for FolderDto will be
-	    * 
-	    * 
-	    * {
-		*  id: integer,
-		*  name: string,
-		*  files: [ File ],
-		*  folders: [ Folder ],
-		* }
-	    * 
-	    * 
-	    * 
-	    */
-	   	FolderDto downloadFolderById(Integer id);
-	   	
-	    List<FolderDto> downloadZipFolder(Integer id);
 
+	IdResponseDto createFolder(FolderUploadDto folderUploadDto);
+
+	FolderDto getFolderById(Integer id);
+
+	/*
+	 * The json response for FolderDto will be
+	 * 
+	 * 
+	 * { id: integer, name: string, files: [ File ], folders: [ Folder ], }
+	 * 
+	 * 
+	 * 
+	 */
+	FolderDto downloadFolderById(Integer id);
+
+	List<FolderDto> downloadZipFolder(Integer id);
 
 }
-
-
-   
