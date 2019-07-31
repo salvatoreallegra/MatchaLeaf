c2555df894f0b1ab2cb9e7dbea0f9c0569de74c6
Thabani Chibanda <tchibanda24@gmail.com>
2019-07-30 21:00:21 -0500

    navbar element style updated

--- client/src/Elements/NavBar/index.js
+++ client/src/Elements/NavBar/index.js
@@ -1,20 +1,15 @@
 import styled from 'styled-components'
 
 const StyledNavBar = styled.nav`
-  grid-row-start: 1;
-  grid-row-end: 5;
-  grid-column-start: 1;
-  grid-column-end: 1;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-  font-family: Roboto;
-  font-style: normal;
+  align-items: flex-start;
+  margin-left: 5vw;
+  margin-bottom: 10vw;
   font-size: 24px;
   line-height: 28px;
-  height: 100vh;
-  border-right: 1px solid #000;
-  margin-top: -10px;
+  width: 15vw;
 `
+
 export default StyledNavBar
