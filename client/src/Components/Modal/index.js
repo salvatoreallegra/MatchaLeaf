7b908c65971b8d2ed227fe4be52d08d95e77fbd5
Thabani Chibanda <tchibanda24@gmail.com>
2019-07-31 13:48:57 -0500

    initial working Modal

--- client/src/Components/Modal/index.js
+++ client/src/Components/Modal/index.js
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import StyledModal from '../../Elements/Modal'
+
+const createFileForm = () => {
+  return (
+    <form method='post' encType='multipart/form-data'>
+      <input type='file' name='file[]' multiple />
+      <input type='submit' value='Upload File' name='submit' />
+    </form>
+  )
+}
+
+const createFolderForm = () => {
+  return (
+    <form method='post' encType='multipart/form-data'>
+      <input type='folder' name='folder' multiple />
+      <input type='submit' value='Create folder' name='submit' />
+    </form>
+  )
+}
+
 
 const Modal = ({ hideModal }) => {
+  const [flag, setFlag] = useState(true)
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-      }}
-    >
-      <form method='post' encType='multipart/form-data'>
-        <input type='file' name='files[]' multiple />
-        <input type='submit' value='Upload File' name='submit' />
-      </form>
-      <button onClick={hideModal}>cancel</button>
-    </div>
+    <StyledModal>
+      <div>
+        <nav
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            width: '100%',
+            height: '8vh',
+            marginBottom: '5vh',
+          }}
+        >
+          <button onClick={ () => setFlag(true) } style={{ width: '50%', backgroundColor: '#FFFFFF' }}>File</button>
+          <button onClick={ () => setFlag(false) } style={{ width: '50%', backgroundColor: '#FFFFFF'  }}>Folder</button>
+        </nav>
+        <div style={{ width: '25vw' }}>
+          { flag ? createFileForm() : createFolderForm() }
+        </div>
+        <button onClick={hideModal}>cancel</button>
+      </div>
+    </StyledModal>
   )
 }
 export default Modal
---
+++ client/src/Elements/Modal/index.js
@@ -0,0 +1,15 @@
+
+import styled from 'styled-components'
+
+const StyledModal = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  font-size: 24px;
+  width: 25vw;
+  height:10vh;
+  width: 100%;
+  height: 100%;
+`
+
+export default StyledModal
