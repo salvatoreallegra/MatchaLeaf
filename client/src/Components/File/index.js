c1dd88fc83b2782851482cb4b73245dad8529573
Thabani Chibanda <tchibanda24@gmail.com>
2019-08-01 11:17:57 -0500

    File/Folder styling

--- client/src/Components/File/index.js
+++ client/src/Components/File/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import fileIcon from '../../image_source/fileIcon.png'
-import downloadIcon from '../../image_source/downloadIcon.png'
-import trashIcon from '../../image_source/trashIcon.png'
-import restoreIcon from '../../image_source/restoreIcon.png'
+import fileIcon from '../../image_source/fileIcon2.png'
+import downloadIcon from '../../image_source/downloadIcon2.png'
+import trashIcon from '../../image_source/trashIcon2.png'
+import restoreIcon from '../../image_source/restoreIcon2.png'
 
 const File = (isTrash, files, folders, toggleTrash, moveOrDeleteFile) => (
   <div>
@@ -15,35 +15,49 @@ const File = (isTrash, files, folders, toggleTrash, moveOrDeleteFile) => (
           boxShadow: '1px 1px 5px rgba(000, 000, 000, 0.2)'
         }}
       >
-        <img src={fileIcon} alt='file' />
-        {file.name}
-        {isTrash ? null : <img src={downloadIcon} alt='download' />}
-        <img
-          src={isTrash ? restoreIcon : trashIcon}
-          alt='trash'
-          onClick={() => toggleTrash(file.id)}
-        />
-        {isTrash ? (
-          <img
-            src={trashIcon}
-            alt='trash'
-            onClick={() => moveOrDeleteFile(file.id)}
-          />
-        ) : null}
-        {isTrash ? null : (
-          <select
-            onChange={event => moveOrDeleteFile(file.id, event.target.value)}
-          >
-            <option value='' defaultValue hidden>
-              Move to
-            </option>
-            {folders.map(folder => (
-              <option value={folder.id} key={folder.id}>
-                {folder.name}
-              </option>
-            ))}
-          </select>
-        )}
+        <span
+          style={{
+            display:'flex',
+            flexDirection:'row',
+            justifyContent: 'space-between'
+          }}>
+          <span>
+            <img style={{ margin: '2px 5px 0 5px' }} src={fileIcon} alt='file' />
+            {file.name}
+          </span>
+          <span>
+            {isTrash ? null : <img style={{ marginTop: '2px' }} src={downloadIcon} alt='download' />}
+            <img
+              style={{ marginTop: '2px' }}
+              src={isTrash ? restoreIcon : trashIcon}
+              alt='trash'
+              onClick={() => toggleTrash(file.id)}
+            />
+            {isTrash ? (
+              <img
+                style={{ marginTop: '2px' }}
+                src={trashIcon}
+                alt='trash'
+                onClick={() => moveOrDeleteFile(file.id)}
+              />
+            ) : null}
+            {isTrash ? null : (
+              <select
+                style={{ border:'none'}}
+                onChange={event => moveOrDeleteFile(file.id, event.target.value)}
+              >
+                <option value='' defaultValue hidden>
+                  Move to
+                </option>
+                {folders.map(folder => (
+                  <option value={folder.id} key={folder.id}>
+                    {folder.name}
+                  </option>
+                ))}
+              </select>
+            )}
+          </span>
+      </span>
       </div>
     ))}
   </div>
--- client/src/Components/Folder/index.js
+++ client/src/Components/Folder/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import folderIcon from '../../image_source/folderIcon.png'
-import downloadIcon from '../../image_source/downloadIcon.png'
-import trashIcon from '../../image_source/trashIcon.png'
-import restoreIcon from '../../image_source/restoreIcon.png'
+import folderIcon from '../../image_source/folderIcon2.png'
+import downloadIcon from '../../image_source/downloadIcon2.png'
+import trashIcon from '../../image_source/trashIcon2.png'
+import restoreIcon from '../../image_source/restoreIcon2.png'
 
 const Folder = (
   isTrash,
@@ -21,44 +21,58 @@ const Folder = (
           boxShadow: '1px 1px 5px rgba(000, 000, 000, 0.2)'
         }}
       >
-        <img src={folderIcon} alt='folder' />
         <span
-          onClick={() => loadFolder(folder.id)}
-          style={{ cursor: 'pointer' }}
-        >
-          {folder.name}
-        </span>
-        {isTrash ? null : <img src={downloadIcon} alt='download' />}
-        <img
-          src={isTrash ? restoreIcon : trashIcon}
-          alt='trash'
-          onClick={() => toggleTrash(folder.id)}
-        />
-        {isTrash ? (
-          <img
-            src={trashIcon}
-            alt='trash'
-            onClick={() => moveOrDeleteFolder(folder.id)}
-          />
-        ) : null}
-        {isTrash ? null : (
-          <select
-            onChange={event =>
-              moveOrDeleteFolder(folder.id, event.target.value)
-            }
-          >
-            <option value='' defaultValue hidden>
-              Move to
-            </option>
-            {folders
-              .filter(iterator => iterator.id !== folder.id)
-              .map(obj => (
-                <option value={obj.id} key={obj.id}>
-                  {obj.name}
+          style={{
+            display:'flex',
+            flexDirection:'row',
+            justifyContent: 'space-between'
+          }}>
+          <span>
+            <img style={{ margin: '2px 5px 0 5px' }} src={folderIcon} alt='folder' />
+            <span
+              onClick={() => loadFolder(folder.id)}
+              style={{ cursor: 'pointer' }}
+            >
+              {folder.name}
+            </span>
+          </span>
+          <span>
+            {isTrash ? null : <img style={{ marginTop: '2px' }} src={downloadIcon} alt='download' />}
+            <img
+              style={{ marginTop: '2px' }}
+              src={isTrash ? restoreIcon : trashIcon}
+              alt='trash'
+              onClick={() => toggleTrash(folder.id)}
+            />
+            {isTrash ? (
+              <img
+                style={{ marginTop: '2px' }}
+                src={trashIcon}
+                alt='trash'
+                onClick={() => moveOrDeleteFolder(folder.id)}
+              />
+            ) : null}
+            {isTrash ? null : (
+              <select
+                style={{ border:'none' }}
+                onChange={event =>
+                  moveOrDeleteFolder(folder.id, event.target.value)
+                }
+              >
+                <option value='' defaultValue hidden>
+                  Move to
                 </option>
-              ))}
-          </select>
-        )}
+                {folders
+                  .filter(iterator => iterator.id !== folder.id)
+                  .map(obj => (
+                    <option value={obj.id} key={obj.id}>
+                      {obj.name}
+                    </option>
+                  ))}
+              </select>
+            )}
+          </span>
+        </span>
       </div>
     ))}
   </div>
